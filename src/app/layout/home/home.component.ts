49540afe4c2648778b53facc3c458378064e16b0
Kasunjith-Bimal <kasunysoft@gmail.com>
2019-07-03 23:40:06 +0530

    chnage home ui

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -8,8 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-
+  images: any[];
+  images1: any[];
+  onImageClicked($event){
+    console.log($event);
+  }
   ngOnInit() {
+    this.images = [];
+    this.images1 = [];
+    this.images.push({source:'assets/images/vechical/v1.jpg', alt:'Car', title:'Vechical'});
+    this.images.push({source:'assets/images/vechical/v2.jpg', alt:'Car', title:'Vechical'});
+    this.images.push({source:'assets/images/vechical/v1.jpg', alt:'Car', title:'Vechical'});
+    this.images.push({source:'assets/images/vechical/v2.jpg', alt:'Car', title:'Vechical'});
+    this.images.push({source:'assets/images/vechical/v1.jpg', alt:'car', title:'Vechical'});
+    this.images.push({source:'assets/images/vechical/v2.jpg', alt:'car', title:'Vechical'});
+
+    this.images1.push({source:'assets/images/cloth/galleria1.jpg', alt:'S', title:'Cloths'});
+    this.images1.push({source:'assets/images/cloth/galleria2.jpg', alt:'T', title:'Cloths'});
+    this.images1.push({source:'assets/images/cloth/galleria3.jpg', alt:'T', title:'Cloths'});
+    this.images1.push({source:'assets/images/cloth/galleria4.jpg', alt:'S', title:'Cloths'});
+    this.images1.push({source:'assets/images/cloth/galleria5.jpg', alt:'M', title:'Cloths'});
+    this.images1.push({source:'assets/images/cloth/galleria6.jpg', alt:'J', title:'Cloths'});
   }
 
 }
--- src/app/layout/home/home.module.ts
+++ src/app/layout/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing';
 import { HomeComponent } from './home.component';
+import {GalleriaModule} from 'primeng/galleria';
 
 
 
 @NgModule({
     imports: [
         CommonModule,
-        HomeRoutingModule
+        HomeRoutingModule,
+        GalleriaModule
     ],
     declarations: [
         HomeComponent
